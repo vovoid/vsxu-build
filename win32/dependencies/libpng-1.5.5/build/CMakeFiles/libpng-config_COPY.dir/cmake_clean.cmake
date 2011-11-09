28575cca348fc73364cc01ca48e3be58d14e7401
Jonatan Wallmander <jaw@eris.vovoid>
2011-11-09 13:21:17 +0100

    - Added build scripts for win32 and linux64

---
+++ win32/dependencies/libpng-1.5.5/build/CMakeFiles/libpng-config_COPY.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/libpng-config_COPY"
+  "bin/libpng-config"
+  "libpng-config"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/libpng-config_COPY.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ win32/dependencies/libpng-1.5.5/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jaw/vsxu/vsxu-dependencies/libpng-1.5.5
+# Build directory: /home/jaw/vsxu/vsxu-dependencies/libpng-1.5.5/build
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
