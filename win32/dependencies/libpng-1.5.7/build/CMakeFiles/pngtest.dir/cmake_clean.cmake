82f60a8b3a4a3a3db15e0fe02359bd68848de10f
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2014-06-29 13:58:51 +0200

    New build stuff for 0.5.0

---
+++ win32/dependencies/libpng-1.5.7/build/CMakeFiles/pngtest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pngtest.dir/pngtest.obj"
+  "pngtest.pdb"
+  "pngtest.exe"
+  "libpngtest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/pngtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ win32/dependencies/libpng-1.5.7/build/CMakeFiles/pngvalid.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/jaw/vsxu/build/win32/dependencies/libpng-1.5.7/contrib/libtests/pngvalid.c" "/home/jaw/vsxu/build/win32/dependencies/libpng-1.5.7/build/CMakeFiles/pngvalid.dir/contrib/libtests/pngvalid.obj"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "PNG_CONFIGURE_LIBPNG"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jaw/vsxu/build/win32/dependencies/libpng-1.5.7/build/CMakeFiles/png15.dir/DependInfo.cmake"
+  )
