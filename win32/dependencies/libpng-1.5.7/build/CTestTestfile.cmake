a145a591dd92512d2606566bb024bd8dc60c3619
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2015-10-20 10:41:19 +0200

    Merged 0.6.1

--- win32/dependencies/libpng-1.5.7/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jaw/vsxu/build/win32/dependencies/libpng-1.5.7
-# Build directory: /home/jaw/vsxu/build/win32/dependencies/libpng-1.5.7/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(pngtest "./pngtest" "/home/jaw/vsxu/build/win32/dependencies/libpng-1.5.7/pngtest.png")
-ADD_TEST(pngvalid "./pngvalid")
