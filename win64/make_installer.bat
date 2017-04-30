f68ce5c36bf055dacabd921996dc91184dd10a31
Jonatan "jaw" Wallmander <jonatan@vovoid.com>
2017-04-30 09:41:54 +0200

    removing builds for 32 bit platforms (not supported anymore)

--- win64/make_installer.bat
+++ win64/make_installer.bat
@@ -5,8 +5,8 @@ md build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR="NMake Makefiles" -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG" -DINSTALL_WIN_LAUNCHER=1 -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /Ob2 /DNDEBUG" ..\..\..\vsxu\
 C:\Qt\qtcreator-4.1.0\bin\jom
-rem upx --ultra-brute programs\artiste\vsxu_artiste.exe
-rem upx --ultra-brute programs\player\vsxu_player.exe
-rem upx --ultra-brute programs\launcher\vsxu_launcher.exe
+upx --ultra-brute programs\artiste\vsxu_artiste.exe
+upx --ultra-brute programs\player\vsxu_player.exe
+upx --ultra-brute programs\launcher\vsxu_launcher.exe
 cpack -G NSIS
 cd ..
